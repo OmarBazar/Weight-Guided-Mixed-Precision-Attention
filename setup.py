60c4f9dbcae5fc2450d75d36c66aaa868b91e1ca
WA225 <walaaa@uci.edu>
2024-11-06 17:28:15 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+# run: python setup.py install
+
+from setuptools import setup, Extension
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import os
+
+os.environ["TORCH_CUDA_ARCH_LIST"] = "8.0"
+setup(
+    name='call_cspmm',
+    ext_modules=[
+        CUDAExtension(
+            'call_cspmm',
+            ['cu_spmm.cpp', 'cuSPMM.cu'],
+        ),
+    ],
+    cmdclass={'build_ext': BuildExtension}
+)
+
+setup(
+    name='call_dummy',
+    ext_modules=[
+        CUDAExtension(
+            'call_dummy',
+            ['cu_dummy.cpp', 'cuSPMM.cu'],
+        ),
+    ],
+    cmdclass={'build_ext': BuildExtension}
+)
